d0ec4e82e593c2064fba8c4e0c9d733236902ab9
Jewel Gapuz <jeweljohngapuz@gmail.com>
2026-03-05 15:27:35 -0600

    done variable practice exercises

---
+++ exercises/month01/week01/day01_variables/variable_practice.py
@@ -0,0 +1,46 @@
+# exercise 1: Create a variable called rent and assign it the value of 1100. Then print the variable.
+rent = 1100
+print(f"Our rent is: {rent}")
+
+# exercise 2: Create two variables called income and expenses. Assign them the values of 3500 and 1500 respectively. Then create a variable called profit that calculates the difference between income and expenses. Finally, print the profit variable.
+income = 3500
+expenses = 1500
+profit = income - expenses
+print(f"income is: {income}")
+print(f"expenses is: {expenses}")
+print(f"Our profit is: {profit}")
+
+# exercise 3: Create a variable called city and assign it the value of "Saskatoon". Then print a sentence that includes the city, rent, and profit variables.
+city = "Saskatoon"
+print(f"Our rent in {city} is {rent} and our saving is {profit} monthly.")
+
+# exercise 4: Create a variable called cash_flow that calculates the difference between income and the sum of rent, gas, and groceries. Assign the appropriate values to the variables and print the cash_flow variable.
+rent = 1100
+gas = 60
+groceries = 400
+
+cash_flow = income - (rent + gas + groceries)
+print(f"Our cash flow is: {cash_flow}")
+
+#speed round
+
+#1
+price = 1000
+print(price)
+#2
+rent = 1200
+yearly_rent = rent * 12
+print(yearly_rent)
+#3
+sales = 5000
+cost = 3000
+profit = sales - cost
+print(profit)
+#4
+average_income = 4000 / 2
+print(average_income)
+#5
+hours_worked = 20
+rate = 15.50
+income = hours_worked * 15.50
+print(income)
